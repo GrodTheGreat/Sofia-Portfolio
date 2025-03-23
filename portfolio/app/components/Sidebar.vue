c7669aa87a0726e4032954f2035b225cd929abe0
Gerard Causse <grod.causse@gmail.com>
2025-03-23 18:37:34 -0400

    Add social links

--- portfolio/app/components/Sidebar.vue
+++ portfolio/app/components/Sidebar.vue
@@ -1,22 +1,51 @@
 <template>
   <div>
     <div class="logo">
+      <img src="#" alt="Logo goes here" />
       <h2>Sofia Escobar</h2>
     </div>
     <nav>
       <ul>
-        <li>Illustration</li>
-        <li>Design</li>
-        <li>About</li>
-        <li>Contact</li>
+        <li>
+          <a href="#">Illustration</a>
+        </li>
+        <li>
+          <a href="#">Design</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
       </ul>
     </nav>
     <div class="socials">
       <ul>
-        <li>Behance</li>
-        <li>Instagram</li>
-        <li>Facebook</li>
-        <li>Email</li>
+        <li>
+          <a href="https://www.behance.net/sofiaescobar_design" target="_blank">
+            Behance
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com/sofiaescobarillustration/profilecard/"
+            target="_blank"
+          >
+            Instagram
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com/share/aBACSoiSTN/?mibextid=wwXlfr"
+            target="_blank"
+          >
+            Facebook
+          </a>
+        </li>
+        <li>
+          <a href="#" target="_blank">Email</a>
+        </li>
       </ul>
     </div>
   </div>
