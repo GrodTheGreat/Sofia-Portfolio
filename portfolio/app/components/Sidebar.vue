5c5e4e3dfd05a7bf362b9e4fb2e5a7f77959a779
Gerard Causse <grod.causse@gmail.com>
2025-03-23 18:25:29 -0400

    Add placeholders for sidebar

--- portfolio/app/components/Sidebar.vue
+++ portfolio/app/components/Sidebar.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>Sofia sidebar</div>
+  <div>
+    <div class="logo">
+      <h2>Sofia Escobar</h2>
+    </div>
+    <nav>
+      <ul>
+        <li>Illustration</li>
+        <li>Design</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+    <div class="socials">
+      <ul>
+        <li>Behance</li>
+        <li>Instagram</li>
+        <li>Facebook</li>
+        <li>Email</li>
+      </ul>
+    </div>
+  </div>
 </template>
